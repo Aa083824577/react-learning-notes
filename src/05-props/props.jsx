c99161f45aa2884a44b15909ec29c13c042eb217
brahim boughezroun <105074830+Aa083824577@users.noreply.github.com>
2025-08-06 12:28:54 +0100

    i add the prop exemple

---
+++ src/05-props/props.jsx
@@ -0,0 +1,12 @@
+const User = (props) =>{
+    return (
+        <section>
+            <img src={props.img} alt={props.name} width={200} />
+            <h1>Name : {props.name}</h1>
+            <h2>Age : {props.age}</h2>
+            <h3>Is isMarried : {props.isMarried}</h3>
+            <h4>Hobbies : {props.hobbies}</h4>
+        </section>
+    )
+}
+export default  User ; 
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import Productlist from './04-list-render/Produitlist'
+import User from './05-props/props'
 function App() {
+const users = [
+  {
+    img: "https://via.placeholder.com/150",
+    name: "pc",
+    age: 17,
+    isMarried: "yes",
+    hobbies: ["coding"]
+  },
+  {
+    img: "https://via.placeholder.com/150",
+    name: "Sara",
+    age: 22,
+    isMarried: "no",
+    hobbies: ["drawing", "traveling"]
+  },
+  {
+    img: "https://via.placeholder.com/150",
+    name: "Ali",
+    age: 28,
+    isMarried: "yes",
+    hobbies: ["gaming", "fitness"]
+  }
+];
   return (
+
     <div>
-      <Productlist/>
-    </div>
+      {users.map((user , index) => (
+      <User
+    
+        img = {user.img}  
+        name = {user.name}
+        age = {user.age}
+        isMarried = {user.isMarried}
+        hobbies = {user.hobbies}
+        />
+      ))}
+      </div>
   )
 }
 
